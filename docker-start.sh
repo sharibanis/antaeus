870c525a00bac44e55acfcde93ab6c0545e96aa7
sharib <sharib.anis@live.com>
2019-08-31 09:44:30 -0400

    Moved the volume comment to before the command

--- docker-start.sh
+++ docker-start.sh
@@ -6,11 +6,11 @@ set -x
 docker build . --tag pleo-antaeus
 
 # Build the code.
+# The volume is only there so incremental builds are way faster
 docker run \
   --publish 7000:7000 \
   --rm \
   --interactive \
   --tty \
-  # This volume is only there so incremental builds are way faster
   --volume pleo-antaeus-build-cache:/root/.gradle \
   pleo-antaeus
